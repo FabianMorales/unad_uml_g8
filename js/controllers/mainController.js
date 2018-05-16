7ae0e38bc3c921c4a432767d0a7a017105abec16
Fabian Morales <hfmorales@elpais.com.co>
2018-05-16 15:05:34 -0500

    Items de menú e íconos

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -13,12 +13,18 @@
     }])
 	.controller('MainController', ['$scope', '$http', '$location', function ($scope, $http, sesion, $location) {
         $scope.menus = [
-            { label: 'Inicio', clase:'fi-torso', href: '#/', activo: false },
-            { label: 'Login', clase:'fi-home', href: '#/login', activo: false },
-            { label: 'Gestión de cargos', clase:'fi-torsos-all', href: '#/cargos', activo: false },
-            { label: 'Gestión de empleados', clase:'fi-torso', href: '#/empleados', activo: false },
-            { label: 'Gestión de turnos', clase:'fi-clipboard-pencil', href: '#/turnos', activo: false },
-            { label: 'Histórico', clase:'fi-clock', href: '#/historico', activo: false },
+            { label: 'Inicio', clase:'fi-home', href: '#/', activo: false },
+            { label: 'Login', clase:'fi-torso', href: '#/login', activo: false },
+            { label: 'Registro de usuarios', clase:'fi-plus', href: '#/login', activo: false },
+            { label: 'Gestión de roles', clase:'fi-torsos-all', href: '#/cargos', activo: false },
+            { label: 'Gestión de permisos', clase:'fi-clipboard-pencil', href: '#/turnos', activo: false },
+            { label: 'Gestión de clientes', clase:'fi-torso', href: '#/empleados', activo: false },
+            { label: 'Gestión de proveedores', clase:'fi-torso', href: '#/empleados', activo: false },
+            { label: 'Gestión de usuarios', clase:'fi-torsos', href: '#/empleados', activo: false },
+            { label: 'Gestión de productos', clase:'fi-book', href: '#/empleados', activo: false },
+            { label: 'Gestión de ventas', clase:'fi-dollar', href: '#/empleados', activo: false },
+            { label: 'Gestión de compras', clase:'fi-shopping-bag', href: '#/empleados', activo: false },
+            { label: 'Catálogo de productos', clase:'fi-shopping-cart', href: '#/empleados', activo: false },
             { label: 'Respaldo base de datos', clase:'fi-database', href: './api/files/backup', activo: false }
         ];
         
